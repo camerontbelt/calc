849d9f419db7e7e48ff980688c01b2ffe647d2b0
Cameron Belt <camerontbelt@gmail.com>
2021-03-08 14:19:49 -0600

    Added a solution and test project.

---
+++ calc.test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using System;
+
+namespace calc.test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void HelloTest()
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
