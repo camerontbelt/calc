84698dbc77ba92707163ff784e041e9bd5319eb0
Cameron Belt <camerontbelt@gmail.com>
2021-03-23 22:52:40 -0500

    Fixed the parser

--- calc/CalcParser.cs
+++ calc/CalcParser.cs
@@ -1,6 +1,6 @@
 public class CalcParser : Parser
 {
-    public CalcParser(CalcLexer lexer) : base(lexer, 3)
+    public CalcParser(CalcLexer lexer) : base(lexer, 1)
     {
     }
 
@@ -12,26 +12,38 @@ public class CalcParser : Parser
 
     public AstNode Expression()
     {
-        var loperand = new AstNode(LookAheadToken(1));
-        Devour();
-        var op = new AstNode(LookAheadToken(1));
-        op.AddChildNode(loperand);
-        Devour();
-        if(LookAhead(2) == TokenTypes.EOF)
+        var term = Term();
+        AstNode expression = null;
+        while(LookAhead(1) == TokenTypes.PLUS || LookAhead(1) == TokenTypes.MINUS)
         {
-            var roperand = new AstNode(LookAheadToken(1));
-            Devour();
-            op.AddChildNode(roperand);
-            return op;
-        } 
-        if(LookAhead(2) == TokenTypes.PLUS 
-        || LookAhead(2) == TokenTypes.MULTIPLY
-        || LookAhead(2) == TokenTypes.DIVIDE
-        || LookAhead(2) == TokenTypes.MINUS)
+            expression = new AstNode(LookAheadToken(1));
+            expression.AddChildNode(term);
+            Devour();// Eat the operator
+            expression.AddChildNode(Expression());
+        }
+        if(expression == null) return term;
+        return expression;
+    }
+
+    public AstNode Term()
+    {
+        var factor = Factor();
+        AstNode term = null;
+        while(LookAhead(1) == TokenTypes.MULTIPLY || LookAhead(1) == TokenTypes.DIVIDE)
         {
-            op.AddChildNode(Expression());
-            return op;
-        } 
-        return null;
+            term = new AstNode(LookAheadToken(1));
+            term.AddChildNode(factor);
+            Devour();// Eat the operator
+            term.AddChildNode(Term());
+        }
+        if(term == null)return factor;
+        return term;
+    }
+
+    public AstNode Factor()
+    {
+        var factor = new AstNode(LookAheadToken(1));
+        Devour();
+        return factor;
     }
 }
--- calc/Program.cs
+++ calc/Program.cs
@@ -6,20 +6,25 @@ namespace calc
     {
         static void Main(string[] args)
         {
-            var text = "1+2*3/4-5";
+            var text = "14 + 2 * 3 - 6 / 2";
+            //var text = "14 + 2 + 6";
             Console.WriteLine("Input\n"+text);
+
             var lexer = new CalcLexer(text);
-            var tokens = lexer.Tokenize();
-            Console.WriteLine("\nTokens");
-            foreach(var token in tokens)
-            {
-                Console.WriteLine(token);
-            }
+            // var tokens = lexer.Tokenize();
+            // Console.WriteLine("\nTokens");
+            // foreach(var token in tokens)
+            // {
+            //     Console.WriteLine(token);
+            // }
+
             var parser = new CalcParser(new CalcLexer(text));
             var tree = parser.Parse();
             Console.WriteLine("\nParse Tree");
             Console.WriteLine(tree.ToStringTree());
+
             var interpreter = new CalcInterpreter();
+            Console.WriteLine("\nResult");
             Console.WriteLine(interpreter.Interpret(tree));
         }
     }
